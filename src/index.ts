98d845bbe95de31063d55a2b131b545cc5cfdb44
ayoub.labite@gmail.com <ayoub.labite@gmail.com>
2025-10-30 09:53:36 +0100

    update the express 4 to express 5

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import {express} from "express";
+import { ApolloServer } from "apollo-server-express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+
+
+dotenv.config();
+
+const startServer = async () => {
+  
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  const PORT = process.env.PORT || 4000;
+
+  const mongoUri = process.env.MONGO_URI;
+  if (!mongoUri) {
+    throw new Error("MONGO_URI environment variable is not set");
+  }
+  await mongoose.connect(mongoUri);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
